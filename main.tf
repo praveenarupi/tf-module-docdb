122d3668beceb4f2d99f05fbd3b27a1b4560cb33
praveenarupi <praveenarupi@gmail.com>
2022-10-20 22:14:29 +0530

    updating ./tf-module-docdb

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier = "${var.env}-docdb"
   engine = "docdb"
+  engine_version = var.engine_version
+  master_username = local.username
+  master_password = local.password
+  skip_final_snapshot = true
+}
 
-
+resource "aws_docdb_subnet_group" "" {
+  subnet_ids = []
 }
